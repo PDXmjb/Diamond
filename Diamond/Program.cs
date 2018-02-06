dfb88d1f33bee3a05511cf7040e19baf6afeacd2
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-02-05 22:17:16 -0800

    Creates a diamond from spaces and stars of any odd-integer size.

--- Diamond/Program.cs
+++ Diamond/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Diamond
+public class Diamond
 {
-    class Program
+    public static string print(int n)
     {
-        static void Main(string[] args)
+        // If even, zero, or negative, return null.
+        if (n % 2 == 0 || n < 0)
         {
+            return null;
         }
+
+        var diamond = new StringBuilder();
+        var center = n / 2 + 1;
+
+        // To simplify later math, using a starting index of 1.
+        for (int i = 1; i <= n; i++)
+        {
+            var line = "";
+
+            // Calculating how far from the middle of the diamond we are.
+            var distanceFromCenter = Math.Abs(center - i);
+
+            // We need to have the number of spaces be the same as how far we are from the middle,
+            // e.g., if we're 2 away from center, we need two spaces.
+            for (int j = 0; j < distanceFromCenter; j++)
+            {
+                line += " ";
+            }
+
+            // The number of stars is equal to the size of the diamond, minus 2(distance from center)
+            // e.g., size = 5, # of stars = 5 - (2 * 2) = 1 for the first row.
+            for (int k = 0; k < (n - (distanceFromCenter * 2)); k++)
+            {
+                line += "*";
+            }
+
+            diamond.Append(line + "\n");
+        }
+
+        return diamond.ToString();
     }
 }
